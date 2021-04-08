42bd3d834c17e822da35620dc8767b370644dc0d
Vansh018 <75937381+Vansh018@users.noreply.github.com>
2021-04-08 21:07:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,3 +25,45 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log('Model Loaded!');
 }
+
+function speak(){
+    var synth = winodw.speechSynthesis;
+    speak_data_1 = "The First Prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error) {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2=results[1].label;
+        speak();
+        if(results[0].label == "amazing")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "Best")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "Victory")
+        {
+            document.getElementById("update_emoji").innerHTML="&#129304;";
+        }
+
+       
+    }
+}
